0a31620653ee04248e280d4815a16e0f5f7ed0cf
unknown <palaksoni1907@gmail.com>
2020-04-03 20:12:07 +0530

    HelloWorld.java

---
+++ src/SumOfDigit.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SumOfDigit {
+
+	public static void main(String[] args) {
+		
+		int sum=0, remainder;
+		int number = 25;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Value for sum of the digit");
+		number = sc.nextInt();
+		
+		while(number>0)
+		{
+			remainder = number % 10;
+			sum = sum + remainder; 	
+			number = number /10; 
+		}
+		
+		System.out.println("Sum of given digit number is = " +sum );
+
+	}
+
+}
