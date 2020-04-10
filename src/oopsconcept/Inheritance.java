aa6dd185d34063d3c7f79c582065b4ed1c949103
unknown <palaksoni1907@gmail.com>
2020-04-10 20:18:34 +0530

    Inheritance.java

--- src/oopsconcept/Inheritance.java
+++ src/oopsconcept/Inheritance.java
@@ -1,6 +1,9 @@
 package oopsconcept;
 
-
+/*Create two classes, A and B, with default constructors (empty argument lists) that announce themselves. 
+Inherit a new class called C from A, and create a member of class B inside C. 
+Do not create a constructor for C. Create an object of class C and observe the results.
+*/
 class A{
 	public A() 
 	{
